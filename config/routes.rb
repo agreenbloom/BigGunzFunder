4ef869746792e8031b8357c098b2d0d38d86e44e
Kevin McLoughlin <kevinmcloughlin2010@gmail.com>
2015-07-22 16:06:28 -0400

    add Rewards controller and nest Reward routing under Projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'rewards/index'
+
+  get 'rewards/new'
+
+  get 'rewards/create'
+
+  get 'rewards/destroy'
+
   root 'users#new'
 
   resources :users, only: [:create, :new, :destroy] do
@@ -11,6 +19,8 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new', :as => :login
   post 'logout' => 'sessions#destroy', :as => :logout
 
-  resources :projects
+  resources :projects do
+    resources :rewards, only: [:index, :create, :new, :destroy]
+  end
 
 end
