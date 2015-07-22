260406b08dc1fee3adeee717584bf003863587a6
Michael Fich <michaeltf@gmail.com>
2015-07-22 16:45:56 -0400

    Added a Pledge controller and amended the Project show view to accept a new pledge on a specified project by the current user to become a backer.

---
+++ app/controllers/pledges_controller.rb
@@ -0,0 +1,27 @@
+class PledgesController < ApplicationController
+  def new
+    @pledge = Pledge.new
+  end
+
+  def create
+    @pledge = Pledge.new(pledge_params)
+    @pledge.project = Project.find(params[:project_id])
+    @pledge.backer = current_user
+    if @pledge.save
+      flash[:notice] = "Thanks for pledging $#{@pledge.amount}0!"
+      redirect_to projects_url
+    else
+      flash[:alert] = "Unable to make pledge.  Please try again."
+      render :new
+    end
+  end
+
+  def show
+    @pledge = Pledge.find(params[:id])
+  end
+
+  private
+  def pledge_params
+    params.require(:pledge).permit(:amount)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,14 @@ Rails.application.routes.draw do
 
   root 'users#new'
 
-  resources :users, only: [:create, :new, :destroy] do
-    resources :pledges, only: [:index, :create, :new, :show]
-  end
-
+  resources :users, only: [:create, :new, :destroy]
   resources :sessions, only: [:create, :new, :destroy]
 
   get 'login' => 'sessions#new', :as => :login
   post 'logout' => 'sessions#destroy', :as => :logout
 
-  resources :projects
+  resources :projects do
+    resources :pledges, only: [:index, :new, :create, :show]
+  end
 
 end
