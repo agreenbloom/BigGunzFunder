5ba36c4d208039e8f5e485874a9020108307a378
Michael Fich <michaeltf@gmail.com>
2015-07-22 15:04:21 -0400

    Corrected issue with automatically logging in user once they register a new account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      login(@user)
+      auto_login(@user)
       redirect_to projects_url
       flash[:notice] = "You've signed up!"
     else
