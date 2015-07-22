47c004bd81a3ee0f98caed9f11b9ddf4439c1133
Kevin McLoughlin <kevinmcloughlin2010@gmail.com>
2015-07-22 00:47:06 -0400

    fix syntax error in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
 resources :users, only: [:create, :new, :destroy] do
-  resources :pledges, only [:index, :create, :new, :show]
+  resources :pledges, only: [:index, :create, :new, :show]
 
 end
