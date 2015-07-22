3bfd7bc2744da5f4f7fe73cff7e7096615ddd70f
Kevin McLoughlin <kevinmcloughlin2010@gmail.com>
2015-07-22 01:02:56 -0400

    add Sessions routing and controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :users, only: [:create, :new, :destroy] do
     resources :pledges, only: [:index, :create, :new, :show]
   end
+
+  resources :sessions, only: [:create, :new, :destroy]
 end
