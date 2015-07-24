d862c1f6596f68d34afaf0631dc872c5494358ed
Kevin McLoughlin <kevinmcloughlin2010@gmail.com>
2015-07-24 14:27:16 -0400

    add start time must be in the future validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,11 +6,22 @@ class Project < ActiveRecord::Base
 
   accepts_nested_attributes_for :rewards, :reject_if => :all_blank, :allow_destroy => true
 
-  validates :goal, numericality: true
+  validates :goal, presence: true, numericality: true
   validates :start_date, presence: true
   validates :end_date, presence: true
+
+  validate :cannot_be_in_the_past
 
   def pledges_by_current_user
     self.pledges.where(backer: @current_user)
   end
+
+  def cannot_be_in_the_past
+    if :start_date.present? && :start_date < Date.today
+      errors.add(:start_date, "cannot be in the past.")
+    end
+  end
+
+
+
 end
