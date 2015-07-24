a3a72ec5f996bb1ff237d8bb4ddcaeeaf3323fa0
Michael Fich <michaeltf@gmail.com>
2015-07-24 16:18:39 -0400

    Added a days remaining method to a project which checks to see the days remaining before a project will begin or the days until the end date if it has already started

--- app/models/project.rb
+++ app/models/project.rb
@@ -14,13 +14,25 @@ class Project < ActiveRecord::Base
   validate :cannot_be_finished_before_start_date
   validate :max_project_duration
 
-  def pledges_by_current_user
-    self.pledges.where(backer: @current_user)
+  def days_remaining
+    if (DateTime.now.utc > self.start_date)
+      (self.end_date.to_date - DateTime.now.to_date).to_i
+    else
+      (self.start_date.to_date - DateTime.now.to_date).to_i
+    end
+  end
+
+  def pledges_by_user(user)
+    self.pledges.where(backer: user)
+  end
+
+  def funds_remaining_to_reach_goal
+    self.goal - self.pledges.sum('amount')
   end
 
   def start_date_cannot_be_in_the_past
-    if :start_date.present? && :start_date < Date.today
-      errors.add(:start_date, "cannot be in the past.")
+    if self.start_date.present? && self.start_date < DateTime.now
+      errors.add(start_date, "cannot be in the past.")
     end
   end
 
@@ -29,15 +41,15 @@ class Project < ActiveRecord::Base
   end
 
   def cannot_be_finished_before_start_date
-    if :end_date.present? && :end_date < :start_date
-      errors.add(:end_date, "cannot finish before start date")
+    if self.end_date.present? && self.end_date < self.start_date
+      errors.add(self.end_date, "cannot finish before start date")
     end
   end
 
   def max_project_duration
-    if :end_date.present? && :start_date.present?
-      if (:end_date - :start_date) > 1.month
-        errors.add(:end_date, "project can't be longer than 1 month long")
+    if self.end_date.present? && self.start_date.present?
+      if (self.end_date - self.start_date) > 1.month
+        errors.add(self.end_date, "project can't be longer than 1 month long")
       end
     end
   end
